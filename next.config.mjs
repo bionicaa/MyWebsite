30126a5262611a30d5833c1a15bc196b32f06890
Amin Abdi <amin.abdi@hotmail.co.uk>
2025-09-18 17:15:22 +0100

    test 4 mdx

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '' : '',
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
