197246c0df74ab8f27534d35406217f104484b09
Amin Abdi <amin.abdi@hotmail.co.uk>
2025-12-08 18:18:46 +0000

    fix analytics

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,39 @@
+// ...existing code...
 import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* load gtag.js in the head at SSR time */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XPGWWCFLCE"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="ga-analytics" strategy="afterInteractive">
+        {/* inline config also in head at SSR time */}
+        <Script id="ga-analytics" strategy="beforeInteractive">
           {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-XPGWWCFLCE');`}
         </Script>
+
         <title>Amin Abdi</title>
         <meta name="description" content="Amin Abdi's personal website" />
         <link rel="icon" href="/icon.png" />
       </head>
       <body>
+        {/* If you're using GTM (not just gtag), include the noscript iframe here:
+            <noscript>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+                      height="0" width="0" style={{display:'none',visibility:'hidden'}} />
+            </noscript>
+         */}
         {children}
       </body>
     </html>
   );
 }
-
+// ...existing code...
 
 
