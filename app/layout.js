836107098110bea367aff8c56c9686e876a68939
Amin Abdi <amin.abdi@hotmail.co.uk>
2025-12-08 17:54:09 +0000

    fix analytics

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
+import Script from 'next/script';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XPGWWCFLCE"></script>
-        <script dangerouslySetInnerHTML={{__html: `
-          window.dataLayer = window.dataLayer || [];
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XPGWWCFLCE"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XPGWWCFLCE');
-        `}} />
+          gtag('config', 'G-XPGWWCFLCE');`}
+        </Script>
         <title>Amin Abdi</title>
         <meta name="description" content="Amin Abdi's personal website" />
         <link rel="icon" href="/icon.png" />
